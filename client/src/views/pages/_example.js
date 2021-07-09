108a64953cdf0a216543f1614965e02f376ff0f6
Maxieprodmoore <binglinho@gmail.com>
2021-07-09 17:11:04 +0800

    First Commit
    Reorganization of files

---
+++ client/src/views/pages/_example.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>App content</h1>
+        <p>Content is added here!</p>
+        <br />
+      </div>
+    </div>
+  );
+}
+
+export default App;
