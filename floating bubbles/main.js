21563dc86897c87661000e6f12f159496d467c79
FinellaM <36837664+FinellaM@users.noreply.github.com>
2021-07-17 17:23:17 +1000

    Update main.js

--- floating bubbles/main.js
+++ floating bubbles/main.js
@@ -1,13 +1,11 @@
 
-gsap.to(".dot", {x: 0}); /* moves to this position on x-axis */
-
-gsap.from('.dot', { y: 750 }); /* from this spot on y-axis*/
-
-
+gsap.from('.dot', { y: 1000, duration: 2}); /* from this spot on y-axis*/
 
 var tl = gsap.timeline({repeat: 30, repeatDelay: 1});
 
 //add 3 tweens that will play in direct succession.
-tl.to(".numberone", {duration: 0.2, opacity: 0, x: 0, y: 20});
-tl.to(".numbertwo", {duration: 0.2,  opacity: 0, x: 0, scale: 0.2, y: 20});
-tl.to(".numberthree", {duration: 0.5,  opacity: 0, x: 0, scale: 2, y: 20});
+ tl.to(".numberone", {duration: 3, opacity: 2, x: 0, y: -1000, scale: 3});
+tl.to(".numbertwo", {duration: 0.1,  opacity: 1, x: 0, scale: 0.2, y: -1000});
+tl.to(".numberthree", {duration: 0.1,  opacity: 1, x: 0, scale: 2, y: -1000});
+
+
