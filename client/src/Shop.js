34486d92feec23bb36eb7e56330f9ba71193b52b
Maxieprodmoore <binglinho@gmail.com>
2021-07-20 15:16:06 +0800

    cleared Shop page

--- client/src/Shop.js
+++ client/src/Shop.js
@@ -5,10 +5,6 @@ const Shop = () => {
 
     return (
         <div className="container text-center">
-            <Link to = "/product-appleRhuharb">Rhubarb & Apple</Link>
-            <Link to = "/product-Mixed">Feel Good Mixed pack</Link>
-            <Link to = "/product-peachPassionfruit">Peach & Passionfruit</Link>
-            <Link to = "/product-raspberryHibiscus">Raspberry & Hibiscus</Link>
         </div>
     );
 }
