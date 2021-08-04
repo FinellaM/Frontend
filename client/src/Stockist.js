76b8fc1882862c4f12f79a461308e81ced1df7bc
Maxieprodmoore <binglinho@gmail.com>
2021-08-04 14:27:22 +0800

    Added to stockist page + images for stockists

--- client/src/Stockist.js
+++ client/src/Stockist.js
@@ -7,10 +7,40 @@ const Stockist = () => {
     }, []);
 
     return (
-        <div className="container text-center">
-            <div class="box green" style={{ backgroundColor: 'red', height: '100px', }}></div>
-            <div class="box orange" style={{ backgroundColor: 'blue', height: '100px', }}></div>
-            <div class="box grey" style={{ backgroundColor: 'white', height: '100px', }}></div>
+        <div className="container-fluid text-center">
+            <div className="col-12 text-left" style={{borderBottom: '3px solid black',
+            paddingBotton: '0.25em',}}>
+                <h1>Our Stockists</h1>
+            </div>
+            <div className="col-12 text-center box green" style={{ backgroundColor: 'red', height: '100px', }}>
+                <h3>Find our products at the following retailers</h3>
+                <div className="row row-lg-3 row-md-2 g-3" >
+                    <img src="./logo_booths.png" alt="" className="col-lg-4 col-md-6" />
+                    <img src="./logo_booths.png" alt="" className="col-lg-4 col-md-6" />
+                    <img src="./logo_booths.png" alt="" className="col-lg-4 col-md-6" />
+                    <img src="./logo_booths.png" alt="" className="col-lg-4 col-md-6" />
+                    <img src="./logo_booths.png" alt="" className="col-lg-4 col-md-6" />
+                    <img src="./logo_booths.png" alt="" className="col-lg-4 col-md-6" />
+                </div>
+            </div>
+            <div className="col-12 text-center box orange" style={{ backgroundColor: 'blue', height: '100px', }}>
+                <h3>Find our products at the following wholesalers</h3>
+                <div className="row row-lg-3 row-md-2 g-2">
+                    <img src="./logo_booths.png" alt="" className="col-lg-4 col-md-6" />
+                    <img src="./logo_booths.png" alt="" className="col-lg-4 col-md-6" />
+                    <img src="./logo_booths.png" alt="" className="col-lg-4 col-md-6" />
+                    <img src="./logo_booths.png" alt="" className="col-lg-4 col-md-6" />
+                    <img src="./logo_booths.png" alt="" className="col-lg-4 col-md-6" />
+                </div>
+            </div>
+            <div className="col-12 text-center box grey" style={{ backgroundColor: 'white', height: '100px', }}>
+
+                <h3 className="gy-5">Want to stock our product?</h3>
+                <br></br>
+                <a type="button" className="btn btn-lg mx-auto" href="/contact-us"><h4>Contact Us</h4></a>
+
+            </div>
+  
         </div>
     );
 }
