7d00630465df65cd57a1c75a444a1899673cf5d1
Maxieprodmoore <binglinho@gmail.com>
2021-08-04 16:32:40 +0800

    Stockist page layout updated

--- client/src/Stockist.js
+++ client/src/Stockist.js
@@ -13,33 +13,33 @@ const Stockist = () => {
             paddingBotton: '0.25em',}}>
                 <h1>Our Stockists</h1>
             </div>
-            <div className="row mx-lg-5 align-items-center text-left">
-                <div className="col-12 text-center mb-3">
-                    <h3 className="my-4">Find our products at the following retailers</h3>
-                    <div className="row row-lg-3 row-md-2 g-3" >
-                        <img src="./logo_booths.png" alt="" className="col-lg-4 col-md-6 mb-sm-4" />
-                        <img src="./logo_booths.png" alt="" className="col-lg-4 col-md-6 mb-sm-4" />
-                        <img src="./logo_booths.png" alt="" className="col-lg-4 col-md-6 mb-sm-4" />
-                        <img src="./logo_booths.png" alt="" className="col-lg-4 col-md-6" />
-                        <img src="./logo_booths.png" alt="" className="col-lg-4 col-md-6" />
-                        <img src="./logo_booths.png" alt="" className="col-lg-4 col-md-6" />
+            <div className="row mx-lg-5 mx-md-2 align-items-center text-left">
+                <div className="col-12 text-center mb-5">
+                    <h3 className="my-5" style={{fontWeight: 'bold'}}>Find our products at the following retailers</h3>
+                    <div className="row g-3" >
+                        <img src="./logo_booths.png" alt="logo of Booths" className="col-lg-4 col-md-6 mb-sm-4" />
+                        <img src="./logo_amazon.png" alt="logo of Amazon" className="col-lg-4 col-md-6 mb-sm-4" />
+                        <img src="./logo_whsmith.png" alt="logo of WH Smith" className="col-lg-4 col-md-6 mb-sm-4" />
+                        <img src="./logo_spar.png" alt="logo of Spar" className="col-lg-4 col-md-6 mb-sm-4" />
+                        <img src="./logo_ocado.png" alt="logo of Ocado" className="col-lg-4 col-md-6 mb-sm-4" />
+                        <img src="./logo_staustell.png" alt="logo of St Austell brewery" className="col-lg-4 col-md-6 mb-sm-4" />
                     </div>
                 </div>
-                <div className="col-12 text-center mb-3">
-                    <h3 className="mb-3 mt-5">Find our products at the following wholesalers</h3>
-                    <div className="row row-lg-3 row-md-2 g-2">
-                        <img src="./logo_bidfood.png" alt="" className="col-lg-4 col-md-6 mb-sm-4" />
-                        <img src="./logo_unnu.png" alt="" className="col-lg-4 col-md-6 mb-sm-4" />
-                        <img src="./logo_jds.png" alt="" className="col-lg-4 col-md-6 mb-sm-4" />
-                        <img src="./logo_diverse.png" alt="" className="col-lg-4 col-md-6" />
-                        <img src="./logo_brakes.png" alt="" className="col-lg-4 col-md-6" />
+                <div className="col-12 text-center mb-5">
+                    <h3 className="mb-3 mt-5" style={{fontWeight: 'bold'}}>Find our products at the following wholesalers</h3>
+                    <div className="row g-2">
+                        <img src="./logo_bidfood.png" alt="logo of Bid food" className="col-lg-4 col-md-6 mb-sm-4" />
+                        <img src="./logo_unnu.png" alt="logo of Unnu" className="col-lg-4 col-md-6 mb-sm-4" />
+                        <img src="./logo_jds.png" alt="logo of JD's" className="col-lg-4 col-md-6 mb-sm-4" />
+                        <img src="./logo_diverse.png" alt="logo of Diverse" className="col-lg-4 col-md-6" />
+                        <img src="./logo_brakes.png" alt="logo of Brakes" className="col-lg-4 col-md-6" />
                     </div>
                 </div>
                 <div className="col-12 text-center mb-5">
 
-                    <h3 className="mb-3 mt-5">Want to stock our product?</h3>
+                    <h3 className="mb-3 mt-5" style={{fontWeight: 'bold'}}>Want to stock our product?</h3>
                     <br></br>
-                    <a type="button" className="btn btn-lg" href="/contact-us"><h4>Contact Us</h4></a>
+                    <a type="button" className="btn btn-lg" href="/contact-us"><h4 style={{fontWeight: 'bold'}}>Contact Us</h4></a>
 
                 </div>
             </div>
