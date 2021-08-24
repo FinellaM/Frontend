10b84d2f98f9e3c01e171ee3e03caf160856d474
FinellaM <s3666910@student.rmit.edu.au>
2021-08-24 16:02:41 +1000

    formatted code correctly

--- client/src/Home.js
+++ client/src/Home.js
@@ -5,41 +5,123 @@ const Home = () => {
     useEffect(() => {
         const gsap = window.gsap;
 
-     
+
         // animate bubbles using gsap
-        gsap.fromTo(".bubbleone", {scale: 0.1},
-        {scale: 1, duration:2,  repeat: -1, opacity: 0})
+        gsap.fromTo(".bubbleone", {
+            scale: 0.1
+        }, {
+            scale: 1,
+            duration: 2,
+            repeat: -1,
+            opacity: 0
+        })
 
-        gsap.fromTo(".bubbletwo", {scale: 1},
-        {scale: 1.5, duration:2,  repeat: -1, opacity:  0})
+        gsap.fromTo(".bubbletwo", {
+            scale: 1
+        }, {
+            scale: 1.5,
+            duration: 2,
+            repeat: -1,
+            opacity: 0
+        })
 
-        gsap.fromTo(".bubblethree", {scale: 0.5, opacity:0},
-        {scale: 1.5, duration:2,  repeat: -1, opacity:1})
+        gsap.fromTo(".bubblethree", {
+            scale: 0.5,
+            opacity: 0
+        }, {
+            scale: 1.5,
+            duration: 2,
+            repeat: -1,
+            opacity: 1
+        })
 
-        gsap.fromTo(".bubblefour", {scale: 0, opacity:1},
-        {scale: 1.5, duration:2,  repeat: -1, opacity:0})
+        gsap.fromTo(".bubblefour", {
+            scale: 0,
+            opacity: 1
+        }, {
+            scale: 1.5,
+            duration: 2,
+            repeat: -1,
+            opacity: 0
+        })
 
         // add timeline 
-        var tl = gsap.timeline({ repeat: -1, repeatDelay: 0 });
+        var tl = gsap.timeline({
+            repeat: -1,
+            repeatDelay: 0
+        });
 
         //add 3 tweens that will play in direct succession.
-        tl.to(".numberone", { duration: 1, scale: 1, opacity: 0});
-        tl.to(".numbertwo", { duration: 1, opacity: 0,  scale: 2});
-        tl.to(".numberthree", { duration: 0.5, opacity: 0, scale: 2 });
-        tl.to(".numberfour", { duration: 0.1, opacity: 0, scale: 2});
-        tl.to(".numberfive", { duration: 1, opacity: 0,  scale: 2});
-        tl.to(".numbersix", { duration: 0.5, scale: 1.5, opacity: 0 });
+        tl.to(".numberone", {
+            duration: 1,
+            scale: 1,
+            opacity: 0
+        });
+        tl.to(".numbertwo", {
+            duration: 1,
+            opacity: 0,
+            scale: 2
+        });
+        tl.to(".numberthree", {
+            duration: 0.5,
+            opacity: 0,
+            scale: 2
+        });
+        tl.to(".numberfour", {
+            duration: 0.1,
+            opacity: 0,
+            scale: 2
+        });
+        tl.to(".numberfive", {
+            duration: 1,
+            opacity: 0,
+            scale: 2
+        });
+        tl.to(".numbersix", {
+            duration: 0.5,
+            scale: 1.5,
+            opacity: 0
+        });
 
         // add another timeline 
-        var t2 = gsap.timeline({ repeat: -1, repeatDelay: 0 });
+        var t2 = gsap.timeline({
+            repeat: -1,
+            repeatDelay: 0
+        });
 
         //add 3 tweens that will play in direct succession.
-        t2.to(".number-one", { duration: 1, opacity: 0,  x: 0});
-        t2.to(".number-two", { duration: 1, opacity: 0, scale: 2 });
-        t2.to(".number-three", { duration: 0.5, opacity: 1, x: 0, scale: 2, y: 20 });
-        t2.to(".number-four", { duration: 0.5, opacity: 0,scale: 2 });
-        t2.to(".number-five", { duration: 1, opacity: 0, scale: 2});
-        t2.to(".number-six", { duration: 0.5, opacity: 0, scale:1.5});
+        t2.to(".number-one", {
+            duration: 1,
+            opacity: 0,
+            x: 0
+        });
+        t2.to(".number-two", {
+            duration: 1,
+            opacity: 0,
+            scale: 2
+        });
+        t2.to(".number-three", {
+            duration: 0.5,
+            opacity: 1,
+            x: 0,
+            scale: 2,
+            y: 20
+        });
+        t2.to(".number-four", {
+            duration: 0.5,
+            opacity: 0,
+            scale: 2
+        });
+        t2.to(".number-five", {
+            duration: 1,
+            opacity: 0,
+            scale: 2
+        });
+        t2.to(".number-six", {
+            duration: 0.5,
+            opacity: 0,
+            scale: 1.5
+        });
 
 
         // animation for class circle objects
@@ -52,7 +134,7 @@ const Home = () => {
             opacity: 2,
             y: '+=10',
             scale: 1.5,
-            
+
         }, {
             opacity: 1,
             y: '-2000', //  moving to this position on y-axis
@@ -255,34 +337,30 @@ const Home = () => {
                         </div>
                         <div className="row bubble-container">
                             <div className="col-6">
-                                <div class="bubbleone"></div>
-                                <div class="bubbletwo"></div>
-                                <div class="bubblethree"></div>
-                                <div class="bubblefour"></div>
-                                <div class="dot number-three"></div>
-                                
-                                
-                                <div class="circles">
-                                    <div class="circle"></div>
-                                    <div class="circle"></div>
-                                    <div class="circle"></div>
-                                    <div class="circle"></div>
+                                    <div class="bubbleone"></div>
+                                    <div class="bubbletwo"></div>
+                                    <div class="bubblethree"></div>
+                                    <div class="bubblefour"></div>
+                                    <div class="dot number-three"></div>
+                                    
+                                    
+                                    <div class="circles">
+                                        <div class="circle"></div>
+                                        <div class="circle"></div>
+                                        <div class="circle"></div>
+                                        <div class="circle"></div>
+                                    </div>
+                                    
                                 </div>
-                                
-                            </div>
                             <div className="col-6">
                                 <div>
-                                    <div class="dot number-two"></div>
+                                <div class="dot number-two"></div>
                                     <div class="dot numberone" ></div>
                                     <div class="dot numberfive" ></div>
                                     <div class="dot numbertwo"></div>
-                                    
                                     <div class=" dot number-one" ></div>
                                     <div class="dot number-five" ></div>
-                                    
-
                                 </div>
-                                
                             </div>
                         </div>
                         <div className="row insta-imgs">
@@ -318,29 +396,18 @@ const Home = () => {
                                 <div class="dot numberthree"></div>
                                 <div class="dot numberfour"></div>
                                 <div class="dot number-six" ></div>
-                            
-                                <div class="dot number-four"></div>
-                                {/* <div class="circles1">
-                                    <div class="circle"></div>
-                                    <div class="circle"></div>
-                                    <div class="circle"></div>
-                                    <div class="circle"></div>
-                                    <div class="circle"></div>
-                                    <div class="circle"></div>
-                                    <div class="circle"></div>
-                                    <div class="circle"></div>
-                                </div> */}
-                                <div class="circles2">
-                                    <div class="circle"></div>
-                                    <div class="circle"></div>
-                                    <div class="circle"></div>
-                                    <div class="circle"></div>
-                                    <div class="circle"></div>
-                                    <div class="circle"></div>
-                                    <div class="circle"></div>
-                                    <div class="circle"></div>
-                                </div>
+                                <div class="dot number-four"></div>    
+                            <div class="circles2">
+                                <div class="circle"></div>
+                                <div class="circle"></div>
+                                <div class="circle"></div>
+                                <div class="circle"></div>
+                                <div class="circle"></div>
+                                <div class="circle"></div>
+                                <div class="circle"></div>
+                                <div class="circle"></div>
                             </div>
+                        </div>
                         </div>
                     </div>
                 </div>
